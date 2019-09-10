bad6f9386e0ea44a4e6faeacff40e58836df9620
xiongmi39 <xiongmi39@163.com>
2019-09-10 09:57:51 +0800

    605

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import "babel-polyfill";
+import Utils from './js/utils/utils.js';
 // import reverseList from './js/linkList/reverseLink.js';
 // import reverseWords from './js/others/reverseWords.js';
 // import pickBinaryStr from './js/binary/binaryString.js';
 // import getPhoneNumComb from './js/array/phoneNumbers.js';
 // import getSubSets from './js/array/subsets.js';
-import cardgroup from './js/array/card914.js';
-import Utils from './js/utils/utils.js';
+// import cardgroup from './js/array/card914.js';
+import plant from './js/array/flowers605.js';
+
 // import dfssearch from './js/dfs/dfs.js';
 // import bfssearch from './js/bfs/bfs.js';
 let func = () => {};
