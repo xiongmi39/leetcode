540c1b713b224c3968d571d5f67575a835f70acb
xiongmi39 <xiongmi39@163.com>
2019-09-11 19:02:46 +0800

    495

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ import Utils from './js/utils/utils.js';
 // import getPhoneNumComb from './js/array/phoneNumbers.js';
 // import getSubSets from './js/array/subsets.js';
 // import cardgroup from './js/array/card914.js';
-import plant from './js/array/flowers605.js';
+// import plant from './js/array/flowers605.js';
+import findPoisonedDuration from './js/array/teemo495.js';
 
 // import dfssearch from './js/dfs/dfs.js';
 // import bfssearch from './js/bfs/bfs.js';
-let func = () => {};
-const NUM = 45;
-let arr = [1, 2, 4];
-let arrB = arr.map(item => item * 2);
 
-// console.log(arrB.includes(8));
-// console.log("new Set(arrB) is ", new Set(arrB));
+ let utils = new Utils();
+
+
+
--- js/utils/utils.js
+++ js/utils/utils.js
@@ -18,6 +18,22 @@ export default class Utils{
 
 			}
 		};
+		this.compare = {'COMPARE_MORETHAN':1,
+		'COMPARE_EQUAL':0,
+		'COMPARE_LESSTHAN':2
+	};
+
+	}
+	// 1:val1>val2,0:val1==val2,2:val1<val2
+	defaultConpareFn(val1,val2){
+		if(val1-val2 ==0){
+			return 0;
+		}else if(val1-val2>0){
+			return 1;
+		}else{
+			return 2;
+		}
+
 	}
 
 }
