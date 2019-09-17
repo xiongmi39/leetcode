9985dda85d9bad875d5a43cd759b4cabd4e93e6a
xiongmi39 <xiongmi39@163.com>
2019-09-17 08:42:36 +0800

    57

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ import Utils from './js/utils/utils.js';
 // import plant from './js/array/flowers605.js';
 // import findPoisonedDuration from './js/array/teemo495.js';
 // import asteroidCollision from './js/array/asteroid735.js';
-import merge from './js/array/merge54.js';
+// import merge from './js/array/merge54.js';
+import insert from './js/array/insert57.js';
 
 // import dfssearch from './js/dfs/dfs.js';
 // import bfssearch from './js/bfs/bfs.js';
---
+++ js/array/insert57.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function(intervals, newInterval) {
+	if(intervals.length == 0){
+		intervals.push(newInterval);
+		return intervals;
+	}
+	if(newInterval.length === 0){
+		intervals.splice(0,0,newInterval);
+		return intervals;
+	}
+	for (var i = 0,len = intervals.length; i < len; i++) {
+		if(newInterval[1]<intervals[0][0] && newInterval[1]<intervals[0][1]){
+			intervals.unshift(newInterval);
+			break;
+		}
+		if(newInterval[0]>intervals[len-1][0] && newInterval[0]>intervals[len-1][1]){
+			intervals.push(newInterval);
+			break;
+		}
+		if(newInterval[0]>intervals[i][1] && newInterval[1]>intervals[i][1]&&newInterval[0]<intervals[i+1][0] && newInterval[1]<intervals[i+1][0]){
+			intervals.splice(i+1,0,newInterval);
+			break;
+		}
+		if(intervals[i][0]<newInterval[0] && intervals[i][1]<newInterval[0]){
+			continue;
+		}
+		let newleft = Math.min(intervals[i][0],intervals[i][1],newInterval[0]);
+		let newright = Math.max(intervals[i][0],intervals[i][1],newInterval[1]);
+		intervals.splice(i,1);
+		len--;
+		i--;
+		newInterval = [newleft,newright];
+		if(!intervals[i+1] ||(newright<intervals[i+1][0] && newright<intervals[i+1][1])){
+			intervals.splice(i+1,0,[newleft,newright]);
+			return intervals;
+		}
+	}
+    return intervals;
+};
+
+let testcase = insert([[1,5]],[6,7]);
+console.log(testcase);
+
+export default insert;
