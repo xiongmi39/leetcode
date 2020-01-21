43e551b20e2479ba404dec1c719e983ba6daa55b
xiongmi39 <xiongmi39@163.com>
2020-01-21 11:06:50 +0800

    121,152，198,53,5,70

--- js/DP/198rob.js
+++ js/DP/198rob.js
@@ -3,24 +3,23 @@
  * @return {number}
  */
 var rob = function(nums) {
-	let len = nums.length;
-	if(len == 0){
+	if(nums.length == 0){
 		return 0;
 	}
-	if(len == 1){
+	if(nums.length == 1){
 		return nums[0];
 	}
-	if(len == 2){
+	if(nums.length == 2){
 		return Math.max(nums[0],nums[1]);
 	}
-	let dp = new Array(len).fill(0);
+	let dp = new Array(nums.length).fill(0);
 	dp[0] = nums[0]; 
 	dp[1] = nums[1];
 	dp[2] = nums[0]+nums[2];
-    for (let i = 3; i < len; i++) {
+    for (let i = 3; i < nums.length; i++) {
     	dp[i] = Math.max(dp[i-2]+nums[i],dp[i-3]+nums[i]);
     }
-    return Math.max(dp[len-1],dp[len-2]);
+    return Math.max(dp[nums.length-1],dp[nums.length-2]);
 };
 
 let testcase = rob([2,1,1,2]);
